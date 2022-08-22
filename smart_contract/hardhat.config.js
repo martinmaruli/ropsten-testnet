d07a01a7061e8e8e8ddde707684ea32455618b88
Martin <martinmaruli55@gmail.com>
2022-08-22 13:39:57 +0700

    add dotenv

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,11 +1,12 @@
+require("dotenv").config();
 require('@nomiclabs/hardhat-waffle');
 
 module.exports = {
   solidity: '0.8.0',
   networks: {
     rinkeby: {
-      url: 'https://eth-rinkeby.alchemyapi.io/v2/_cd9Bl4sg4ZMbhBhiv3iE6xJrBsYSeVZ',
-      accounts: ['162f2ddb62afe185468423fd0be4966d69ca75847a394c74c09579b39d881d02'],
+      url: process.env.PROVIDER_KEY,
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
 };
