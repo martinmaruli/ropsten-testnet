7dfcebff0a10e552996f5658b039a43d2fe8e3fc
Martin <martinmaruli55@gmail.com>
2022-08-22 13:00:50 +0700

    change testnet to rinkeby temporary

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from "./Transactions.json";
 
-export const contractAddress = "0xfCCF80344a668b72ac4Be23513F0E9E4a35C84fA";
+export const contractAddress = "0xEf331D7002185C100Cf6A10C5620bB326243B5a8";
 export const contractABI = abi.abi;
